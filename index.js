450827743f60762eddfb357121e756591c7b761b
Rodrigo Romualdo <rodrigo.romualdo@intelliway.com.br>
2020-09-23 19:26:13 -0300

    2 commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-<h1>FUNCIONOU </h1>
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res) {
+  
+  res.send("FUNCIONANDO");
+})
+
+
+
+
+
+
+var port = 3000;
+app.listen(port);
+console.log('Umbler - Express server started on port %s', port);
