11bc007e8c2da7960a71f64964ade5e4a0a7b8b2
Rodrigo Romualdo <rodrigo.romualdo@intelliway.com.br>
2020-09-23 19:34:36 -0300

    3 commit

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const handlebars = require('express-handlebars')
+
+
+// Config
+  // Template Engine"
+  app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+  app.set('view engine', 'handlebars')
 
 app.get("/", function(req, res) {
   
-  res.send("FUNCIONANDO");
+  res.render('teste');
 })
 
 
