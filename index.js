59c04f113c7abad67193a83b01af9622b274dfde
Rodrigo Romualdo <rodrigo.romualdo@intelliway.com.br>
2020-09-23 20:21:40 -0300

    5 commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars')
-const SpeedApi = require("./service/SpeedApi")
+const SpeedApi = require("./service/speedApi")
 
 
 // Config
